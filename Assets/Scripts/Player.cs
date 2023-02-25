efc9a508e69b7feb181519e9f2abedd17eeb808d
CarlosPC <santanavargasc@gmail.com>
2023-02-25 02:05:15 -0500

    Cinemachine, 2 player diferente input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody2D rB;
-    [SerializeField] float xSpeed;
-    float xMove;
+    /* Script comentado para coparar commits en Git */
+    Rigidbody2D rB; // Variable que almacera el Rigidbody2D
+    [SerializeField] float xSpeed; //  Velocidad de movimiento
+    float xMove; // Guarda el input en esta variable
+    [SerializeField] private string horizontal;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        rB = GetComponent<Rigidbody2D>();
+        rB = GetComponent<Rigidbody2D>(); // Trae el RB de desde unity
     }
 
-    // Update is called once per frame
     void Update()
     {
-        xMove = Input.GetAxisRaw("Horizontal");
+        // xMove    
+        xMove = Input.GetAxisRaw(horizontal);
         rB.velocity = new Vector2(xMove * xSpeed, rB.velocity.y);
     }
 }
